488d59bee754f7fe90d71bf498d641d02eff25b2
Bùi Thành Nhân <hi@imnhan.com>
2020-11-27 21:31:37 +0700

    cleaner output, more concurrency

--- main.py
+++ main.py
@@ -13,30 +13,30 @@ def fetch_proxy_list() -> List[str]:
     client = urllib3.PoolManager(headers=default_headers)
 
     resp = client.request("GET", "https://www.sslproxies.org/")
-    soup = BeautifulSoup(resp.data)
+    soup = BeautifulSoup(resp.data, "html.parser")
     txt = soup.find("textarea").text.strip()
-    return ["https://" + line for line in txt.split("\n")[3:]]
+    # TODO: use https once urllib3 is updated on arch:
+    # https://github.com/urllib3/urllib3/issues/1850
+    return ["http://" + line for line in txt.split("\n")[3:]]
 
 
 def check_proxy(addr: str) -> bool:
     proxy = urllib3.ProxyManager(addr, headers=default_headers)
     try:
-        ping = proxy.request("GET", "https://httpbin.org/get", timeout=5, retries=False)
+        ping = proxy.request(
+            "GET",
+            "https://hi.imnhan.com/ping.html",
+            timeout=5,
+            retries=False,
+        )
+        return ping.status == 200
     except urllib3.exceptions.HTTPError:
-        print(addr, "failed.")
-        return False
-
-    if ping.status == 200:
-        print(addr, "SUCCESS.")
-        return True
-    else:
-        print(addr, "returns non-200 which should never happen.")
         return False
 
 
 def check_proxies(addrs: List[str]) -> List[str]:
     working_addrs = []
-    with ThreadPoolExecutor(max_workers=20) as executor:
+    with ThreadPoolExecutor(max_workers=40) as executor:
         future_to_addr = {executor.submit(check_proxy, addr): addr for addr in addrs}
         for future in as_completed(future_to_addr):
             addr = future_to_addr[future]
@@ -46,10 +46,13 @@ def check_proxies(addrs: List[str]) -> List[str]:
 
 
 def main():
+    print("Fetching proxy list")
     unchecked_proxies = fetch_proxy_list()
+
+    print("Checking proxies")
     working_proxies = check_proxies(unchecked_proxies)
 
-    print("Working proxies:")
+    print(f"Found {len(working_proxies)} working proxies:")
     for pr in working_proxies:
         print(pr)
 
